312eb7b9b76bd789793d363b3390ac6a4beda6e5
njgibbon <njgibbon@outlook.com>
2019-01-04 01:22:39 +0000

    added new junit for test execution order and meat of navigation tests

--- README.md
+++ README.md
@@ -1,11 +1,36 @@
 # pareture-selenium
-Selenium starter template and test cases for my basic static website: https://pareture.xyz
 
-TODO...
+# description
+Selenium starter template and test cases for my basic static website: https://pareture.xyz. Driven using JUnit5. <br />
+Note I am using the latest JUnit Release Candidate as I wanted to Order Method execution.
 
-# resources
-Chrome Driver:
-http://chromedriver.chromium.org/getting-started
 
-## test-context
-wrote using ItelliJ on a 2017 MacBook.
+# test suite and test execution reporting
+https://github.com/njgibbon/pareture-test <br />
+( TODO: link to actual test suite once docs created. ) <br />
+
+# logic
+One single test suite is executed. Chrome is the chosen browser. <br />
+Before tests Chrome Driver is started and afterwards it is closed. <br />
+The site is a static SPA. <br />
+Each test Opens an internal / external link on the page. <br />
+Once there it does some basic validation that the new page is as expected. <br />
+It then moves on. <br />
+
+
+# execute 
+```
+mvn test -Dtest=TestSuite1
+```
+
+## resources
+Chrome Driver: <br />
+http://chromedriver.chromium.org/getting-started <br />
+
+<br />
+
+Test Exection Order: <br />
+https://junit.org/junit5/docs/5.4.0-M1/user-guide/index.html#writing-tests-test-execution-order <br />
+
+## context
+Wrote using ItelliJ on a 2017 MacBook.
--- src/test/java/com/njgibbon/test/selenium/pareture/suites/TestSuite1.java
+++ src/test/java/com/njgibbon/test/selenium/pareture/suites/TestSuite1.java
@@ -3,6 +3,10 @@ package com.njgibbon.test.selenium.pareture.suites;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -14,6 +18,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 //Do a quick validation to check that the page is the correct target
 //Validation doesn't need to be extensive as content checking is done by other suites
 //Links will also have been verified during static analysis and exploratory test phases
+@TestMethodOrder(OrderAnnotation.class)
 public class TestSuite1
 {
     private static WebDriver driver;
@@ -33,14 +38,206 @@ public class TestSuite1
         driver.quit();
     }
 
-    //Hit the site and do some checks on the page
+    //Hit the with no 'www' and check returns correctly
     @Test
+    @Order(1)
     public void test1() throws InterruptedException
     {
-        driver.get("https://www.pareture.xyz");
-        Thread.sleep(5000);  //wait 5s
-        assertTrue(driver.getPageSource().contains("CVhg"));
+        System.out.println("test 1");
+        System.out.println("load pareture site without www");
+        driver.get("https://pareture.xyz");
+        Thread.sleep(1000);  //wait 1s
+
+        //some validation
+        assertTrue(driver.getPageSource().contains("Site"));
+        assertTrue(driver.getPageSource().contains("Writing"));
+        assertTrue(driver.getPageSource().contains("Code"));
+        assertTrue(driver.getPageSource().contains("CV"));
+        assertTrue(driver.getPageSource().contains("Social"));
     }
 
+    //Hit the with 'www' and check returns correctly
+    @Test
+    @Order(2)
+    public void test2() throws InterruptedException
+    {
+        System.out.println("test 2");
+        System.out.println("load pareture site with www");
+        driver.get("https://www.pareture.xyz");
+        Thread.sleep(1000);  //wait 1s
+
+        //some validation
+        assertTrue(driver.getPageSource().contains("Site"));
+        assertTrue(driver.getPageSource().contains("Writing"));
+        assertTrue(driver.getPageSource().contains("Code"));
+        assertTrue(driver.getPageSource().contains("CV"));
+        assertTrue(driver.getPageSource().contains("Social"));
+
+        //xpath to this site link
+        System.out.println("click self internal link");
+        driver.findElement(By.xpath("/html/body/div/main/div[2]/a")).click();
+        Thread.sleep(1000);  //wait 1s
+
+        //same validation
+        assertTrue(driver.getPageSource().contains("Site"));
+        assertTrue(driver.getPageSource().contains("Writing"));
+        assertTrue(driver.getPageSource().contains("Code"));
+        assertTrue(driver.getPageSource().contains("CV"));
+        assertTrue(driver.getPageSource().contains("Social"));
+    }
+
+    //Hit the external blog link
+    @Test
+    @Order(3)
+    public void test3() throws InterruptedException
+    {
+        System.out.println("test 3");
+        System.out.println("load pareture site with www as starting point");
+        driver.get("https://www.pareture.xyz");
+        Thread.sleep(1000);  //wait 1s
+
+        //xpath to blog link
+        System.out.println("click blog external link");
+        driver.findElement(By.xpath("/html/body/div/main/div[3]/a[1]")).click();
+        Thread.sleep(1000);  //wait 1s
+
+        //some validation
+        //assert here
+    }
+
+    //Hit the external blog archive link
+    @Test
+    @Order(4)
+    public void test4() throws InterruptedException
+    {
+        System.out.println("test 4");
+        System.out.println("load pareture site with www as starting point");
+        driver.get("https://www.pareture.xyz");
+        Thread.sleep(1000);  //wait 1s
+
+        //xpath to blog link
+        System.out.println("click blog archive external link");
+        driver.findElement(By.xpath("/html/body/div/main/div[3]/a[2]")).click();
+        Thread.sleep(1000);  //wait 1s
+
+        //some validation
+        //assert here
+    }
+
+    //Hit the external github link
+    @Test
+    @Order(5)
+    public void test5() throws InterruptedException
+    {
+        System.out.println("test 5");
+        System.out.println("load pareture site with www as starting point");
+        driver.get("https://www.pareture.xyz");
+        Thread.sleep(1000);  //wait 1s
+
+        //xpath to blog link
+        System.out.println("click github external link");
+        driver.findElement(By.xpath("/html/body/div/main/div[5]/a")).click();
+        Thread.sleep(1000);  //wait 1s
+
+        //some validation
+        //assert here
+    }
+
+    //Hit the external linkedin link
+    @Test
+    @Order(6)
+    public void test6() throws InterruptedException
+    {
+        System.out.println("test 6");
+        System.out.println("load pareture site with www as starting point");
+        driver.get("https://www.pareture.xyz");
+        Thread.sleep(1000);  //wait 1s
+
+        //xpath to blog link
+        System.out.println("click linkedin external link");
+        driver.findElement(By.xpath("/html/body/div/main/div[7]/a")).click();
+        Thread.sleep(1000);  //wait 1s
+
+        //some validation
+        //assert here
+    }
+
+    //Hit the external twitter link
+    @Test
+    @Order(7)
+    public void test7() throws InterruptedException
+    {
+        System.out.println("test 7");
+        System.out.println("load pareture site with www as starting point");
+        driver.get("https://www.pareture.xyz");
+        Thread.sleep(1000);  //wait 1s
+
+        //xpath to blog link
+        System.out.println("click twitter external link");
+        driver.findElement(By.xpath("/html/body/div/main/div[9]/a[1]")).click();
+        Thread.sleep(1000);  //wait 1s
+
+        //some validation
+        //assert here
+    }
+
+    //Hit the external facebook link
+    @Test
+    @Order(8)
+    public void test8() throws InterruptedException
+    {
+        System.out.println("test 8");
+        System.out.println("load pareture site with www as starting point");
+        driver.get("https://www.pareture.xyz");
+        Thread.sleep(1000);  //wait 1s
+
+        //xpath to blog link
+        System.out.println("click fb external link");
+        driver.findElement(By.xpath("/html/body/div/main/div[9]/a[2]")).click();
+        Thread.sleep(1000);  //wait 1s
+
+        //some validation
+        //assert here
+    }
+
+    //Hit the internal resources link
+    @Test
+    @Order(9)
+    public void test9() throws InterruptedException
+    {
+        System.out.println("test 9");
+        System.out.println("load pareture site with www as starting point");
+        driver.get("https://www.pareture.xyz");
+        Thread.sleep(1000);  //wait 1s
+
+        //xpath to blog link
+        System.out.println("click resource internal link");
+        driver.findElement(By.xpath("/html/body/footer/a[1]")).click();
+        Thread.sleep(1000);  //wait 1s
+
+        //some validation
+        //assert here
+        assertTrue(driver.getPageSource().contains("A list of the most significant material used on the site or in the creation of the site"));
+    }
+
+    //Hit the internal sitemap link
+    @Test
+    @Order(10)
+    public void test10() throws InterruptedException
+    {
+        System.out.println("test 10");
+        System.out.println("load pareture site with www as starting point");
+        driver.get("https://www.pareture.xyz");
+        Thread.sleep(1000);  //wait 1s
+
+        //xpath to blog link
+        System.out.println("click sitemap internal link");
+        driver.findElement(By.xpath("/html/body/footer/a[2]")).click();
+        Thread.sleep(1000);  //wait 1s
+
+        //some validation
+        //assert here
+        assertTrue(driver.getPageSource().contains("<loc>https://www.pareture.xyz/index.html</loc>"));
+    }
 
 }
